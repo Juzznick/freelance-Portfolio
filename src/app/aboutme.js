470d52ce0709c0ba7cdef628b87b1d2a65fddf4d
Nikhil <79796884+Juzznick@users.noreply.github.com>
2025-04-19 00:03:04 +0530

    updated font

--- src/app/aboutme.js
+++ src/app/aboutme.js
@@ -1,20 +1,17 @@
 const AboutMe = () => {
-    return(
+    return (
         <div className="h-screen bg-white flex justify-center items-center flex-col">
             <h1 className="text-blue-300 text-[15rem]">ABOUTME</h1>
-            <h5 className="text-blue-300 text-3xl w-3/4 font-sans">I am a passionate UI/UX designer and front-end developer focused on
-creating seamless, user-centered digital experiences. With a strong
-foundation in both design and coding, I specialize in building visually
-appealing, responsive interfaces that prioritize functionality. My expertise in
-tools like Figma, strengthened through certifications like 'User Interface
-Design with Figma' (85% score), allows me to craft clean, intuitive designs.
-
-
-
-In addition to design, my proficiency in HTML, CSS, and JavaScript enables
-me to translate concepts into fully functional websites. My experience spans
-responsive web design, user interaction, and performance optimization,
-ensuring that my work is both innovative and technically sound.</h5>
+            <h5 className="text-blue-300 text-2xl w-3/4 description-text">I am a passionate UI/UX designer and front-end developer focused on
+                creating seamless, user-centered digital experiences. With a strong
+                foundation in both design and coding, I specialize in building visually
+                appealing, responsive interfaces that prioritize functionality. My expertise in
+                tools like Figma, strengthened through certifications like 'User Interface
+                Design with Figma' (85% score), allows me to craft clean, intuitive designs.
+                In addition to design, my proficiency in HTML, CSS, and JavaScript enables
+                me to translate concepts into fully functional websites. My experience spans
+                responsive web design, user interaction, and performance optimization,
+                ensuring that my work is both innovative and technically sound.</h5>
         </div>
     );
 }
