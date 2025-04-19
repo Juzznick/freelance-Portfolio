7cebc5a903f2f19269cf61fb0c977f4adc720e7e
Nikhil <79796884+Juzznick@users.noreply.github.com>
2025-04-19 18:36:43 +0530

    navbar

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,11 +1,11 @@
 const NavBar = () => {
     return(
-        <nav className="bg-white flex justify-center items-center pt-2">
-            <ul className="flex justify-around w-screen">
-                <a><li className="cursor-pointer">HOME</li></a>
-                <a><li className="cursor-pointer">ABOUT ME</li></a>
-                <a><li className="cursor-pointer">EDUCATION</li></a>
-                <a><li className="cursor-pointer">EXPERIENCE</li></a>
+        <nav className="bg-white flex justify-center items-center pt-2 h-screen w-1/8 fixed left-0 z-999">
+            <ul className="flex flex-col justify-around h-1/2">
+                <a><li className="cursor-pointer text-5xl text-blue-300">HOME</li></a>
+                <a><li className="cursor-pointer text-5xl text-blue-300">ABOUT ME</li></a>
+                <a><li className="cursor-pointer text-5xl text-blue-300">EDUCATION</li></a>
+                <a><li className="cursor-pointer text-5xl text-blue-300">EXPERIENCE</li></a>
             </ul>
         </nav>
     );
