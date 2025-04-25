1d00d4d1f220ba507b26674b9daacd411fb4641a
Nikhil <79796884+Juzznick@users.noreply.github.com>
2025-04-26 00:52:25 +0530

    nav btns

--- src/app/contact.js
+++ src/app/contact.js
@@ -1,7 +1,10 @@
 const Contact = () => {
     return(
-        <div className="h-screen v-screen flex justify-center">
-            <h1>Contact</h1>
+        <div className="h-screen bg-white flex justify-center items-center flex-col" id="contact">
+            <h1 className="text-primary text-[15rem] reveal">CONTACT ME</h1>
+            <h5 className="text-primary text-2xl w-3/4 description-text reveal">
+                Contact Info Placemarker Contact Info Placemarker Contact Info Placemarker Contact Info Placemarker Contact Info Placemarker
+            </h5>
         </div>
     );
 }
--- src/app/navbar.js
+++ src/app/navbar.js
@@ -4,10 +4,10 @@ const NavBar = () => {
             <div className="bg-primary/50 h-29/30 w-9/10 rounded-2xl flex flex-col items-center justify-center overflow-hidden relative">
                 <img src='/profile-img.png' className="absolute top-20 w-3/4"/>
                 <ul className="flex flex-col items-center w-full">
-                    <a href='#hero' className="w-full flex justify-center items-center bg-primary h-13 m-1"><li className="cursor-pointer text-2xl  text-white">Home</li></a>
-                    <a href='#aboutme' className="w-full flex justify-center  items-center bg-primary h-13 m-1"><li className="cursor-pointer text-2xl  text-white">About Me</li></a>
-                    <a href='#education' className="w-full flex justify-center  items-center bg-primary h-13 m-1"><li className="cursor-pointer text-2xl  text-white">Education</li></a>
-                    <a href='#hero' className="w-full flex justify-center  items-center bg-primary h-13 m-1"><li className="cursor-pointer text-2xl  text-white">Contact</li></a>
+                    <a href='#hero' className="w-full flex justify-center items-center bg-primary h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">Home</li></a>
+                    <a href='#aboutme' className="w-full flex justify-center  items-center bg-primary h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">About Me</li></a>
+                    <a href='#education' className="w-full flex justify-center  items-center bg-primary h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">Education</li></a>
+                    <a href='#contact' className="w-full flex justify-center  items-center bg-primary  h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">Contact</li></a>
                 </ul>
             </div>
         </nav>
