8b91b3fb136ffeac7ad0cf69abeece565355ef11
Nikhil <79796884+Juzznick@users.noreply.github.com>
2025-04-25 23:07:20 +0530

    changed image

--- src/app/hero.js
+++ src/app/hero.js
@@ -3,7 +3,7 @@ export default function Hero(){
         <div className="h-screen relative overflow-hidden flex flex-col items-center right-0" id="hero">
             <h5 className="text-5xl absolute top-15 text-white">Hey, I'm</h5>
             <h1 className="flex items-center justify-center text-[18rem] text-white">NIDA SHIRIN</h1>
-            <img className="absolute bottom-0 object-cover z-40" draggable='false' src='nida-hero.png' />
+            <img className="absolute bottom-0 object-cover z-40" draggable='false' src='nida-anime-hero.png' />
         </div>
     );
 }
