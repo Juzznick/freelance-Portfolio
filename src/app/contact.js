dd08fd5a923a1503efc0e783d6024b80590671ff
juzznick <xnick126@gmail.com>
2025-05-04 23:55:04 +0530

    static reveal for contact

--- src/app/contact.js
+++ src/app/contact.js
@@ -1,7 +1,7 @@
 const Contact = () => {
     return (
         <div className="h-screen bg-white flex justify-center items-center flex-col" id="contact">
-            <h1 className="text-primary text-[15rem] reveal">CONTACT ME</h1>
+            <h1 className="text-primary text-[15rem]">CONTACT ME</h1>
             <h5 className="text-primary flex justify-center text-2xl w-3/4 description-text">
                 +91 7559964498
             </h5>
