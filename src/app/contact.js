55b7da88e76add033dfeb6d5038a156cf4ea2cbd
juzznick <xnick126@gmail.com>
2025-05-06 23:56:54 +0530

    color tweaks

--- src/app/contact.js
+++ src/app/contact.js
@@ -2,10 +2,10 @@ const Contact = () => {
     return (
         <div className="h-screen bg-white flex justify-center items-center flex-col" id="contact">
             <h1 className="text-primary text-[15rem]">CONTACT ME</h1>
-            <h5 className="text-primary flex justify-center text-2xl w-3/4 description-text">
+            <h5 className="text-black/50 flex justify-center text-2xl w-3/4 description-text">
                 +91 7559964498
             </h5>
-            <h5 className="text-primary flex justify-center text-2xl w-3/4 description-text">nidashirin@gmail.com</h5>
+            <h5 className="text-black/50 flex justify-center text-2xl w-3/4 description-text">nidashirin@gmail.com</h5>
 
             <div className="flex w-50 justify-between mt-5">
                 <a href="https://www.linkedin.com/in/nida-shirin/" target="_blank">
