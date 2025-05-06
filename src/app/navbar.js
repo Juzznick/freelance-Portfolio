cae8d660e96ab9e63073aec702c3a7cdb6ba9464
juzznick <xnick126@gmail.com>
2025-05-06 22:23:57 +0530

    changed nav text size

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,14 +1,14 @@
 const NavBar = () => {
     return (
         <nav className="bg-white flex justify-center items-center pt-2 h-screen w-1/6 sticky top-0 z-999 ">
-            <div className="bg-primary/50 h-29/30 w-9/10 rounded-2xl flex flex-col items-center justify-center overflow-hidden relative">
+            <div className="bg-primary h-29/30 w-9/10 rounded-2xl flex flex-col items-center justify-center overflow-hidden relative">
                 {/* <img src='/profile-img.jpeg' className="absolute top-10 w-3/4 rounded-full"/> */}
                 <ul className="flex flex-col items-center w-full">
-                    <a href='#hero' className="w-full flex justify-center items-center bg-primary h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">Home</li></a>
-                    <a href='#aboutme' className="w-full flex justify-center  items-center bg-primary h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">About Me</li></a>
-                    <a href='#education' className="w-full flex justify-center  items-center bg-primary h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">Education</li></a>
-                    <a href='#contact' className="w-full flex justify-center  items-center bg-primary  h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">Contact</li></a>
-                    <a href='/Nida_Shirin_Resume.pdf' className="w-full flex bg-cyan-200 justify-center items-center h-13 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-2xl  text-white">My Resume</li></a>
+                    <a href='#hero' className="w-full flex justify-center items-center bg-primary h-7 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-1xl  text-white">Home</li></a>
+                    <a href='#aboutme' className="w-full flex justify-center  items-center bg-primary h-7 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-1xl  text-white">About Me</li></a>
+                    <a href='#education' className="w-full flex justify-center  items-center bg-primary h-7 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-1xl  text-white">Education</li></a>
+                    <a href='#contact' className="w-full flex justify-center  items-center bg-primary  h-7 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-1xl  text-white">Contact</li></a>
+                    <a href='/Nida_Shirin_Resume.pdf' className="w-full flex bg-cyan-200 justify-center items-center h-7 m-1 shadow-[inset_0px_0px_38px_-1px_rgba(236,_72,_153,_0.5)]"><li className="cursor-pointer text-1xl  text-white">My Resume</li></a>
                 </ul>
             </div>
         </nav>
