5aa838d76b6d98d9d05ffd545c02b67021835313
juzznick <xnick126@gmail.com>
2025-05-07 15:44:15 +0530

    changed color scheme

--- src/app/contact.js
+++ src/app/contact.js
@@ -2,21 +2,21 @@ import Image from "next/image";
 const Contact = () => {
     return (
         <div className="h-screen bg-white flex justify-center items-center flex-col" id="contact">
-            <h1 className="text-primary text-[15rem]">LET&#39;S CHAT <span className="text-secondary">!</span></h1>
-            <h5 className="text-secondary flex justify-center text-2xl w-3/4 description-text">
+            <h1 className="text-secondary text-[15rem]">LET&#39;S CHAT <span className="text-primary">!</span></h1>
+            <h5 className="text-secondary/80 flex justify-center text-2xl w-3/4 description-text">
                 +91 7559964498
             </h5>
-            <h5 className="text-secondary flex justify-center text-2xl w-3/4 description-text">nidashirin@gmail.com</h5>
+            <h5 className="text-secondary/80 flex justify-center text-2xl w-3/4 description-text">nidashirin@gmail.com</h5>
 
             <div className="flex w-50 justify-between mt-5">
                 <a href="https://www.linkedin.com/in/nida-shirin/" target="_blank">
-                    <Image src='/Logos/linkedin.png' alt="linkedin" width="60" height="60"  />
+                    <Image src='/Logos/linkedin.svg' alt="linkedin" width="60" height="60"  />
+                </a>
+                <a href="https://www.instagram.com/iamnida____/"  target="_blank">
+                    <Image width="60" height="60" src="/Logos/instagram.svg" alt="resume" />
                 </a>
                 <a href="/Nida_Shirin_Resume.pdf"  target="_blank">
-                    <Image width="60" height="60" src="/Logos/file.png" alt="resume" />
-                </a>
-                <a href="/Nida_Shirin_Resume.pdf"  target="_blank">
-                    <Image width="60" height="60" src="/Logos/github.png" alt="github" />
+                    <Image width="60" height="60" src="/Logos/github.svg" alt="github" />
                 </a>
             </div>
 
--- src/app/page.js
+++ src/app/page.js
@@ -66,21 +66,8 @@ export default function Home() {
 
 
       <a href='#hero'>
-        <button className='fixed right-5 bottom-5 border-2 border-black/50 rounded-3xl p-2 bg-white/30 cursor-pointer'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='size-6'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18'
-            />
-          </svg>
+        <button className='fixed right-5 bottom-5 border-2 border-white/50 rounded-3xl p-2 bg-white/30 cursor-pointer'>
+        <img width="25" height="25" src="https://img.icons8.com/ios-filled/50/FFFFFF/long-arrow-up.png" alt="long-arrow-up"/>
         </button>
       </a>
     </div>
