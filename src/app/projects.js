5dcf0aabdeb110702a71edbe0b399135cf473306
juzznick <xnick126@gmail.com>
2025-05-18 22:44:43 +0530

    projects

--- src/app/projects.js
+++ src/app/projects.js
@@ -4,27 +4,27 @@ const Projects = () => {
         <div className="h-screen relative overflow-hidden flex flex-col items-center right-0" id="projects">
                     <h1 className="flex items-center justify-center text-[18rem] text-white">PROJECTS</h1>
                     <div className="flex justify-around w-full flex-col sm:flex-row">
-                        <div className="flex justify-center card relative w-75 rounded-2xl h-80 bg-card3 overflow-hidden">
+                        <div className="flex justify-center card relative w-75 rounded-2xl h-80 bg-white/70 overflow-hidden cursor-pointer">
                             <h4 className="flex justify-center absolute bottom-2 ">Planto</h4>
-                            <div className="flex justify-center absolute top-5 h-50">
+                            <div className="flex justify-center absolute top-5 h-50 hover:scale-110">
                                 <Image className="object-cover rounded-2xl" src='/proj1.png' width={250} height={100} alt="project 1"/>
                             </div>
                         </div>
-                        <div className="flex justify-center card relative w-75 rounded-2xl h-80 bg-card3 overflow-hidden">
+                        <div className="flex justify-center card relative w-75 rounded-2xl h-80 bg-white/70 overflow-hidden cursor-pointer">
                             <h4 className="flex justify-center absolute bottom-2 ">Devlabs</h4>
-                            <div className="flex justify-center absolute top-5 h-50">
+                            <div className="flex justify-center absolute top-5 h-50 hover:scale-110">
                                 <Image className="object-cover rounded-2xl" src='/proj2.png' width={250} height={100} alt="project 1"/>
                             </div>
                         </div>
-                        <div className="flex justify-center card relative w-75 rounded-2xl h-80 bg-card3 overflow-hidden">
+                        <div className="flex justify-center card relative w-75 rounded-2xl h-80 bg-white/70 overflow-hidden cursor-pointer">
                             <h4 className="flex justify-center absolute bottom-2 ">BEIN</h4>
-                            <div className="flex justify-center absolute top-5 h-50">
+                            <div className="flex justify-center absolute top-5 h-50 hover:scale-110">
                                 <Image className="object-cover rounded-2xl" src='/proj3.png' width={250} height={100} alt="project 1"/>
                             </div>
                         </div>
-                        <div className="flex justify-center card relative w-75 rounded-2xl h-80 bg-card3 overflow-hidden">
+                        <div className="flex justify-center card relative w-75 rounded-2xl h-80 bg-white/70 overflow-hidden cursor-pointer">
                             <h4 className="flex justify-center absolute bottom-2 ">Airwave</h4>
-                            <div className="flex justify-center absolute top-5 h-50">
+                            <div className="flex justify-center absolute top-5 h-50 hover:scale-110">
                                 <Image className="object-cover rounded-2xl" src='/proj4.png' width={250} height={100} alt="project 1"/>
                             </div>
                         </div>
